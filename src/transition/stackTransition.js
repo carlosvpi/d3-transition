4d3029bed524f7269e0b4eb0a1535e66144a4034
Carlos Vazquez <carlos.vazpi@gmail.com>
2024-11-11 11:42:38 +0100

    remove comments

--- src/transition/stackTransition.js
+++ src/transition/stackTransition.js
@@ -1,28 +1,19 @@
-export default function (arcs) {
+export default function stackTransition(data) {
+  const total = d3.sum(data)
+  const scales = d3.stack()
+    .keys([...data.keys()])
+    ([data])
+    .map(([[start, end]]) => d3.scaleLinear([start/total, end/total], [0, 1]))
+
   return function (element) {
     const ease = element.ease()
-    const duration = element.duration()
-    const data = []
-    arcs.forEach(slice => data.push({ slice, length: slice.data, start: data[data.length - 1]?.end ?? 0, end: (data[data.length - 1]?.end ?? 0) + slice.data }))
-    const sum = d3.sum(data, d => d.length)
-    const scales = data.map(({ start, end }) => d3.scaleLinear([start/sum, end/sum], [0, 1]))
     element.delay(0)
-      .duration(duration)
-      .easeVarying((v, i, slices) => t => {
+      .duration(element.duration())
+      .easeVarying((_, i, slices) => t => {
         let variation = scales[i](ease(t))
-        if (variation <= 0 && i > 0) {
-          d3.select(slices[i]).attr('opacity', 0)
-        }
-        if (variation > 0) {
-          d3.select(slices[i]).attr('opacity', 1)
-        }
-        if (variation > 1 && i < slices.length - 1) {
-          variation = 1
-        }
+        if (variation <= 0 && i > 0) return 0
+        if (variation > 1 && i < slices.length - 1) return 1
         return variation
       })
-      d3.timeout(() => {
-        element.nodes().forEach(slice => d3.select(slice).attr('opacity', 1))
-      }, duration);
   }
 }
